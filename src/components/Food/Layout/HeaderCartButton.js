5290905b0a0542412c7c57f196002ffd70315cba
parkjisu0107 <parkjisu0107@gmail.com>
2023-11-15 12:09:27 +0900

    Cart 담기 기능 및 Modal 렌더링, Cart 화면에서 +-버튼 처리

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './HeaderCartButton.module.scss';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = ({ onShow }) => {
   const { button, icon, badge } = styles;
+
+  const { items } = useContext(CartContext);
+
+  const numberOfCart = items.reduce((accum, item) => {
+    return accum + item.amount;
+  }, 0);
+
   return (
     <button
       className={button}
@@ -15,7 +23,7 @@ const HeaderCartButton = ({ onShow }) => {
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      <span className={badge}>{numberOfCart}</span>
     </button>
   );
 };
